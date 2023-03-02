423f54e1c132fe03e22d6e8a9c775cae96e79e88
Renata Costa <renata_rayanne_@hotmail.com>
2023-03-02 08:04:06 -0300

    feat/create the entire structure of the project

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import {app} from './app';
+import {AppDataSource} from './data-source';
+
+const PORT: number = 3000;
+const runningMsg: string = `Server running on http://localhost:${PORT}`;
+
+AppDataSource.initialize().then(() => {
+
+app.listen(PORT, async () => {
+  console.log('Database connected')
+  console.log(runningMsg)
+})
+}).catch(err => {
+  console.log(err)
+})
+
